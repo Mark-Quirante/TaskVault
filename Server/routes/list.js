9fbcd9fdbae4734d57b9c36c1772e6967bd5f301
Mark Quirante <quir0054@algonquinlive.com>
2024-08-23 18:19:57 -0400

    Added Database Connection

--- Server/routes/list.js
+++ Server/routes/list.js
@@ -1,9 +1,13 @@
 import express from "express";
+import db from "../db/connection.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	res.send("Hello").status(200);
+	let collection = await db.collection("tasks");
+	let results = await collection.find({}).toArray();
+	res.send(results).status(200);
 });
 
 export default router;
